f15f157a7a59fdbff4a5c53c17d9119d621d1593
eeyaaAdwaith <157117562+Adw-eeyaa@users.noreply.github.com>
2024-03-07 23:42:44 +0530

    Add files via upload

---
+++ friend.java
@@ -0,0 +1,12 @@
+public class friend  {
+    double speed;
+    private String model;
+    void go(){
+        System.out.println("this vehicle is moving");
+    }
+    public String getModel(){
+        model = "bnruh";
+        return model;
+
+    }
+}
